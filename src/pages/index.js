27b3952df732d333e8786b857871d5d105f9db1d
Peter Minarik <peter@peterminarik.com>
2018-04-03 11:35:55 +0200

    Init with Gatsby starter

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from 'gatsby-link';
+
+export default () =>
+  <div>
+    Hello worlds!
+    <p>
+      <img src="https://source.unsplash.com/random/400x200" width="400" height="200" />
+    </p>
+    <Link to="/page-2/">Page 2</Link>
+  </div>
