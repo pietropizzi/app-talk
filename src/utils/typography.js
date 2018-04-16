8342daf6e6a8e0ec839539b22b8c51158559f3fe
Peter Minarik <peter@peterminarik.com>
2018-04-16 11:17:03 +0200

    Configure Typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,33 @@
 import Typography from "typography";
 
-const typography = new Typography({ baseFontSize: "18px" });
+const typography = new Typography({
+  baseFontSize: "16px",
+  baseLineHeight: 1.5,
+  scaleRatio: 2,
+  googleFonts: [
+    {
+      name: 'Rubik',
+      styles: [
+        '300',
+        '500',
+      ],
+    },
+    {
+      name: 'Roboto',
+      styles: [
+        '400'
+      ],
+    },
+  ],
+  headerFontFamily: ['Rubik', 'sans-serif'],
+  bodyFontFamily: ['Roboto', 'sans-serif'],
+  headerWeight: '500',
+  headerColor: '#4a4a4a',
+  bodyColor: '#4a4a4a',
+  overrideStyles: ({ adjustFontSizeTo }) => ({
+    h2: adjustFontSizeTo('30px'),
+    h3: adjustFontSizeTo('24px')
+  })
+});
 
 export default typography;
