948ccd1a34971beeda04031820661819ea88e585
Peter Minarik <peter@peterminarik.com>
2018-04-23 15:47:16 +0200

    Links in header and empty about page

--- src/components/header.js
+++ src/components/header.js
@@ -5,14 +5,16 @@ export default () => {
   return (
     <div styleName='container'>
       <div className='g-module'>
-        <h1 styleName='logo'>AppTalk</h1>
+        <h1>
+          <a styleName='logo' href='/'>AppTalk</a>
+        </h1>
         <div styleName='content'>
           <h2 styleName='title'>Let iOS apps talk</h2>
           <div className='g-type-thin' styleName='introText'>
-            A directory of iOS apps that can be launched via URLs including ones that support x-callback-url. <a href=''>Learn more ></a>
+            A directory of iOS apps that can be launched via URLs including ones that support x-callback-url. <a href='/about'>Learn more&nbsp;></a>
           </div>
           <div className='g-type-thin' styleName='explainerText'>
-            Is an app / URL missing? Did you find an error? Help me keep the documentation up-to-date. <a href=''>See how to contribute ></a>
+            Is an app / URL missing? Did you find an error? Help me keep the documentation up-to-date. <a href='/about'>See how to contribute&nbsp;></a>
           </div>
         </div>
       </div>
---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className='g-module'>
+      <h1>About</h1>
+    </div>
+  );
+}
