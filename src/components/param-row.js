3d7088984c0560f91ea5998cbf356ff1317c45f1
Peter Minarik <peter@peterminarik.com>
2018-04-23 15:14:45 +0200

    list for param options and more colors

--- src/components/param-row.js
+++ src/components/param-row.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './param-row.module.css';
 
 const colors = [
-  '#506680',
-  '#508075',
-  '#80507F'
+  '#2A5280',
+  '#3F8273',
+  '#802A7F',
+  '#80722A',
+  '#802A46',
+  '#44802A'
 ];
 
-const getColor = (index) => colors[index] || colors[0];
+const getColor = (index) => colors[index % colors.length];
 
 const ParamRow = ({ param, index }) => {
   return (
@@ -20,7 +23,13 @@ const ParamRow = ({ param, index }) => {
       </div>
       <div styleName='description'>
         { param.description && <div>{param.description}</div> }
-        { param.options && param.options.map((option) => <div key={option.description}>{option.description}</div>) }
+        { param.options &&
+          <ul styleName='options'>
+            { param.options.map((option) =>
+              <li key={option.description}>{option.description}</li>
+            )}
+          </ul>
+        }
       </div>
     </div>
   );
