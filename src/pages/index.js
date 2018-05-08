a41a1e82d983b564ac2fbbc650b88e366277360b
Peter Minarik <peter@peterminarik.com>
2018-05-08 17:50:04 +0200

    Add Favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,6 +35,10 @@ export default ({ data: { site, apps: { edges: appEdges }, appIcons: { edges: ic
         <meta property='og:url' content={url} />
         <meta name='twitter:card' content='summary_large_image' />
         <meta name='twitter:site' content='@pietropizzi' />
+
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='96x96' href='/favicon-96x96.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
       </Helmet>
       <Header metaData={site.siteMetadata} />
       <JumpToApps apps={apps} />
