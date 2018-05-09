f8d8852639c2c52b1e31d6d1a1d153acbbb6ecfa
Peter Minarik <peter@peterminarik.com>
2018-05-09 10:34:24 +0200

    Add an app schema

---
+++ src/schema/app.js
@@ -0,0 +1,48 @@
+export default {
+  type: 'object',
+  required: true,
+  properties: {
+    info: {
+      type: 'object',
+      required: true,
+      properties: {
+        identifier: { type: 'string', required: true },
+        name: { type: 'string', required: true },
+        appstoreUrl: { type: 'string', required: true },
+        documentationUrl: { type: 'string', required: true }
+      }
+    },
+    actions: {
+      type: 'array',
+      required: true,
+      items: {
+        type: 'object',
+        required: true,
+        properties: {
+          name: { type: 'string', required: true },
+          description: { type: 'string' },
+          url: { type: 'string', required: true },
+          parameters: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                name: { type: 'string', required: true },
+                description: { type: 'string' },
+                options: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      description: { type: 'string', required: true }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
