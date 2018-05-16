d39fe93d1d6152bffd9622ea2062cd57f481e9bb
Peter Minarik <peter@peterminarik.com>
2018-05-16 16:48:45 +0200

    Add title to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Header from '../components/header';
 import Intro from '../components/intro';
+import { Helmet } from 'react-helmet';
 
 export default ({ data: { site  }}) => {
+  const { siteMetadata: { title, tagLine }} = site;
+  const fullTitle = `${title} - ${tagLine}`;
+
   return (
     <div>
+      <Helmet>
+        <title>{fullTitle}</title>
+      </Helmet>
       <Header isSmall metaData={site.siteMetadata} />
       <h1 className='g-module g-headline-seperator'>About</h1>
       <Intro />
