d539887ee9fee90136a61715c1297f60820a193b
Peter Minarik <peter@soundcloud.com>
2023-02-06 11:29:17 +0100

    add plausible script

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,6 +49,8 @@ export default class IndexPage extends React.Component {
           <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
           <link rel='icon' type='image/png' sizes='96x96' href='/favicon-96x96.png' />
           <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+
+          <script defer data-domain="app-talk.com" src="/js/script.js"></script>
         </Helmet>
         <Header metaData={site.siteMetadata} />
         <JumpToApps apps={apps} />
