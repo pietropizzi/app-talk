9dfffb15ef1cb1bd696e6f0a37fd69eb76ac1c57
Peter Minarik <peter@soundcloud.com>
2023-02-07 15:03:21 +0100

    remove clicky trackgin

--- src/html.js
+++ src/html.js
@@ -9,18 +9,6 @@ if (process.env.NODE_ENV === `production`) {
   }
 }
 
-const clickyTrackingCode = `
-  var clicky_site_ids = clicky_site_ids || [];
-  clicky_site_ids.push(101116027);
-  (function() {
-    var s = document.createElement('script');
-    s.type = 'text/javascript';
-    s.async = true;
-    s.src = '//static.getclicky.com/js';
-    ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
-  })();
-`;
-
 module.exports = class HTML extends React.Component {
   render() {
     let css
@@ -52,15 +40,6 @@ module.exports = class HTML extends React.Component {
             dangerouslySetInnerHTML={{ __html: this.props.body }}
           />
           {this.props.postBodyComponents}
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{ __html: clickyTrackingCode }}
-          />
-          <noscript>
-            <p>
-              <img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101116027ns.gif" />
-            </p>
-          </noscript>
         </body>
       </html>
     )
